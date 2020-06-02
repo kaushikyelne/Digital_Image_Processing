978ed364cf5558036cce1c4672099a6b3489c79d
Kaushik Yelne <42184951+kaushikyelne@users.noreply.github.com>
2020-06-02 20:35:24 +0530

    Update RGB_Layers.m

--- RGB_Layers.m
+++ RGB_Layers.m
@@ -1,14 +1,24 @@
 clc; close all; clear;
+I = imread('landscape.jpg');
+subplot(2,2,1);
+imshow(uint8(I));
+title('Original Image')
 
-I = imread('ironman.jpg');
+R=I;
+R(:,:,[2 3])= 0;
+subplot(2,2,2);
+imshow(uint8(R));
+title('RED layer Removed Image')
 
-r = I(:,:,1);
-g = I(:,:,2);
-b = I(:,:,3);
+G=I;
+G(:,:,[1 3])= 0;
+subplot(2,2,3);
+imshow(uint8(G));
+title('GREEN layer Removed Image')
 
-subplot(3,1,1)
-imshow(r);
-subplot(3,1,2)
-imshow(g);
-subplot(3,1,3)
-imshow(b);
+B=I;
+B(:,:,[1 2]) = 0;
+subplot(2,2,4);
+imshow(uint8(B));
+
+title('BLUE layer Removed Image')
